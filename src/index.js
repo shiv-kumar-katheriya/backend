4d73487a4401365886ad9b1ba11ebcf58ebbf0cf
Shiv Kumar Katheriya <kumarkumararjun00z@gmail.com>
2024-03-05 22:07:51 +0530

    api response and error configuration inside utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 
 
 dotenv.config({
@@ -8,5 +8,18 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error occured after DB connected successfully ",error);
+        throw error;
+    });
+    let port = process.env.PORT || 8000
+    app.listen(port,()=>{
+        console.log(`Server is running at port ${port} `);
+    })
+})
+.catch(err =>{
+    console.log("Error Occured While connecting to the database : ",err);
+} )
 
